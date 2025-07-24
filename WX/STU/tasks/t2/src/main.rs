2f53492d55616f609cd345bc240d3844b1b97244
K0unty <119057948+K0unty@users.noreply.github.com>
2025-07-24 14:44:18 +0000

    🫦

--- WX/STU/tasks/t2/src/main.rs
+++ WX/STU/tasks/t2/src/main.rs
@@ -1,3 +1,7 @@
+// -------------------------------------------------
+// Main entry point - this is copied diretly from task 2
+// -------------------------------------------------
+
 fn main() {
     println!("Hello, world!");
 }

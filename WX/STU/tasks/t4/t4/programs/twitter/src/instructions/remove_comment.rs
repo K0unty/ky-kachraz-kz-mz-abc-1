bb54a0d0ae2ff887f4d7dc920a7ae5f58465866c
K0unty <119057948+K0unty@users.noreply.github.com>
2025-08-07 03:51:22 +0000

    🫦

--- WX/STU/tasks/t4/t4/programs/twitter/src/instructions/remove_comment.rs
+++ WX/STU/tasks/t4/t4/programs/twitter/src/instructions/remove_comment.rs
@@ -20,13 +20,24 @@ pub fn remove_comment(_ctx: Context<RemoveCommentContext>) -> Result<()> {
 }
 
 #[derive(Accounts)]
+#[instruction(content: String)]
 pub struct RemoveCommentContext<'info> {
     #[account(
         mut,
+        seeds = [
+            COMMENT_SEED.as_bytes(),
+            comment_author.key().as_ref(),
+            &hash(content.as_bytes()).to_bytes(),
+            tweet.key().as_ref()
+        ],
+        bump = comment.bump,
         has_one = comment_author,
         close = comment_author
     )]
     pub comment: Account<'info, Comment>,
+    
+    #[account(mut)]
+    pub tweet: Account<'info, Tweet>,
     
     pub comment_author: Signer<'info>,
 }

49070de5a0323971b0592126e9f7ffb8c73ee70c
K0unty <119057948+K0unty@users.noreply.github.com>
2025-08-10 19:04:48 +0000

    🫦

--- WX/STU/prac/tut/t1/tests/t1.ts
+++ WX/STU/prac/tut/t1/tests/t1.ts
@@ -1,16 +1,23 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { T1 } from "../target/types/t1";
+import * as anchor from "@coral-xyz/anchor"
+import { Program } from "@coral-xyz/anchor"
+import { T1 } from "../target/types/t1"
 
 describe("t1", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  anchor.setProvider(anchor.AnchorProvider.env())
 
-  const program = anchor.workspace.t1 as Program<T1>;
+  const program = anchor.workspace.t1 as Program<T1>
+
+  const signer = anchor.web3.Keypair.generate()
 
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
-});
+    const tx = await program.methods
+      .initialize("Smeling Woman Ass")
+      .accounts({
+        signer: program.provider.wallet.publicKey,
+      })
+      .rpc()
+    console.log("Your transaction signature", tx)
+  })
+})

3f1ed950fadde3cee82ed2c90b8981ad8a818b2f
K0unty <119057948+K0unty@users.noreply.github.com>
2025-08-10 19:21:38 +0000

    🫦

--- WX/STU/prac/tut/t1/tests/t1.ts
+++ WX/STU/prac/tut/t1/tests/t1.ts
@@ -8,26 +8,30 @@ describe("t1", () => {
 
   const program = anchor.workspace.t1 as Program<T1>
 
-  const signer = anchor.web3.Keypair.generate()
-  const dataAccount = anchor.web3.Keypair.generate()
+  // Load existing wallet instead of generating new keypairs
+  const signer = anchor.web3.Keypair.fromSecretKey(
+    new Uint8Array(
+      JSON.parse(
+        require("fs").readFileSync("./solana_wallets/wallet_0.json", "utf-8")
+      )
+    )
+  )
 
   it("Is initialized!", async () => {
-    program.provider.connection.confirmTransaction(
-      // await program.provider.connection.requestAirdrop(
-      //   signer.publicKey,
-      //   1000000000
-      // ),
-      "raped"
-    )
-
     // Add your test here.
     const tx = await program.methods
-      .initialize("Smeling Woman Ass")
+      .initialize("Test Initialization")
       .accounts({
         signer: signer.publicKey,
-        dataAccount: dataAccount.publicKey,
+        // Using program derived address for data account to avoid rent issues
+        dataAccount: (
+          await anchor.web3.PublicKey.findProgramAddress(
+            [Buffer.from("data_account")],
+            program.programId
+          )
+        )[0],
       })
-      .signers([signer, dataAccount])
+      .signers([signer])
       .rpc()
     console.log("Your transaction signature", tx)
   })
